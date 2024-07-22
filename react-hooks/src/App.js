8a87cfed4bd622e56d07f426351c9be7160ccb6c
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-07-22 19:41:26 +0900

    usePreventLeave

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,26 +1,22 @@
 import { useEffect, useState } from "react";
 
-const useConfirm = (message="", callback, rejection) => {
-  if (typeof callback !== "function") {
-    return;
-  }
-  const confirmAction = () => {
-    if(confirm(message)) {
-      callback();
-    } else {
-      rejection();
-    }
-  }
-  return confirmAction
-}
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listener);
+  return {enablePrevent, disablePrevent}
+};
 
 function App() {
-  const deleteWorld = () => console.log("Deleting the world...")
-  const abort = () => console.log("Aborted")
-  const confirmDelete = useConfirm("Are you sure", deleteWorld, abort)
+  const { enablePrevent, disablePrevent } = usePreventLeave();
   return (
     <div>
-      <button onClick={confirmDelete} >Delete the world</button>
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>Unprotect</button>
     </div>
   );
 }

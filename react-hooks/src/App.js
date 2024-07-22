eac11e3e17bd5394ca7bf75a0c6ed491789b0826
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-07-22 19:26:51 +0900

    useConfirm

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,30 +1,26 @@
 import { useEffect, useState } from "react";
 
-const useClick = (onclick) => {
-  if (typeof onClick !== "function") {
+const useConfirm = (message="", callback, rejection) => {
+  if (typeof callback !== "function") {
     return;
   }
-  const element = useRef();
-  useEffect(() => {
-    if (element.current) {
-      element.current.addEventListener("click", onClick);
+  const confirmAction = () => {
+    if(confirm(message)) {
+      callback();
+    } else {
+      rejection();
     }
-    return () => {
-      if (element.current) {
-        element.current.removeEventListenser("click", onclick);
-      }
-    };
-  }, []);
-  return element;
-};
+  }
+  return confirmAction
+}
 
 function App() {
-  const sayHello = () => console.log("say hello");
-  const title = useClick(sayHello);
-  setTimeout(() => console.log(potato), 5000);
+  const deleteWorld = () => console.log("Deleting the world...")
+  const abort = () => console.log("Aborted")
+  const confirmDelete = useConfirm("Are you sure", deleteWorld, abort)
   return (
     <div>
-      <h1 ref={title} >Hi</h1>
+      <button onClick={confirmDelete} >Delete the world</button>
     </div>
   );
 }

927bef9d84b69353f6040b5cfc1b4b2507d0f223
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-08-17 19:50:14 +0900

    useEffect

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
+import useAxios from "./useAxios";
 
 function App() {
-  const begForLife = () => {
-    console.log("Pls don't leave");
-  };
-  useBeforeLeave(begForLife);
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://yts.mx/api/v2/list_movies.json",
+  });
+  console.log(`Loading: ${loading}\nData: ${JSON.stringify(data)}\nError: ${error}`);
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
     </div>
   );
 }
